70c7bd53d6ff22c88bad0fd170b1a95e3ac72fc0
Larissa Porto <larissa.silva86@gmail.com>
2019-12-18 15:40:36 -0300

    fixed 1st rspec

--- spec/features/user_visits_page_spec.rb
+++ spec/features/user_visits_page_spec.rb
@@ -5,6 +5,7 @@ feature 'Visitor opens homepage' do
         visit root_path
         expect(page).to have_content('Jobs')        
         expect(page).to have_content('A página que une candidatos para o emprego dos sonhos')
-        expect(page).to have_link('Entrar')        
+        expect(page).to have_link('Login Candidato')
+        expect(page).to have_link('Login Headhunter')
     end
 end
