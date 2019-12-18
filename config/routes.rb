f37f5dfca3c9d7b80642d3e59c15645e3f2c4cf2
Larissa Porto <larissa.silva86@gmail.com>
2019-12-18 15:37:29 -0300

    devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :headhunters
+  devise_for :applicants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+feature 'applicant sign in' do
+    scenario 'successfully' do
+        applicant = Applicant.create!(email: 'test@test.com', password: '123456')
+
+        visit root_path
+        click_on 'Login Candidato'
+        fill_in 'Email', with: applicant.email
+        fill_in 'Senha', with: applicant.password
+        click_on 'Log in'
+        
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('Login efetuado com sucesso.')
+        expect(page).to have_link('Sair')
+        expect(page).not_to have_link('Login Candidato')
+    end
+
+    scenario 'and log out' do
+        applicant = Applicant.create!(email: 'test@test.com', password: '123456')
+
+        visit root_path
+        click_on 'Login Candidato'
+        fill_in 'Email', with: applicant.email
+        fill_in 'Senha', with: applicant.password
+        click_on 'Log in' 
+        click_on 'Sair'
+        
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('Logout efetuado com sucesso.')
+        expect(page).to have_link('Login Candidato')
+        expect(page).not_to have_link('Sair')
+    end
+end
+feature 'headhunter sign in' do
+    scenario 'successfully' do
+        headhunter = Headhunter.create!(email: 'test@test.com', password: '123456')
+
+        visit root_path
+        click_on 'Login Headhunter'
+        fill_in 'Email', with: headhunter.email
+        fill_in 'Senha', with: headhunter.password
+        click_on 'Log in'
+        
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('Login efetuado com sucesso.')
+        expect(page).to have_link('Sair')
+        expect(page).not_to have_link('Login Headhunter')
+    end
+
+    scenario 'and log out' do
+        headhunter = Headhunter.create!(email: 'test@test.com', password: '123456')
+
+        visit root_path
+        click_on 'Login Headhunter'
+        fill_in 'Email', with: headhunter.email
+        fill_in 'Senha', with: headhunter.password
+        click_on 'Log in' 
+        click_on 'Sair'
+        
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('Logout efetuado com sucesso.')
+        expect(page).to have_link('Login Headhunter')
+        expect(page).not_to have_link('Sair')
+    end
+end
