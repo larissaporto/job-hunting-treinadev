44dfb69436d4b9e53d0ce8057c68b93e5b7e45ab
Larissa Porto <larissa.silva86@gmail.com>
2019-12-21 11:50:44 -0300

    before new job_level

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,12 @@
 class JobsController < ApplicationController
-    before_action :authenticate_headhunter, only: [:new, :create]
+    before_action :authenticate_headhunter!, only: [:new, :create]
     before_action :authorize_headhunter, only: [:new, :create]
 
     def index 
     end
 
     def new 
+        @job = Job.new
     end
 
     def create
@@ -14,6 +15,6 @@ class JobsController < ApplicationController
     private
 
     def authorize_headhunter
-        redirect_to root_path unless current_headhunter?
+        redirect_to root_path unless current_headhunter
     end
 end
