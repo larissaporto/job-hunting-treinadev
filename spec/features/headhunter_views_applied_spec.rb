b817c39b3492795898bc5ee2cad9b0d238eff524
Larissa Porto <larissa.silva86@gmail.com>
2019-12-25 20:59:07 -0300

    some tests

--- spec/features/headhunter_views_applied_spec.rb
+++ spec/features/headhunter_views_applied_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'Headhunter see all that applied to the job' do
     scenario 'successfully' do
         headhunter = Headhunter.create!(email: 'test@test.com', password: '123456')
+        otherheadhunter = Headhunter.create!(email: 'test1@test.com', password: '123456')
         applicant = Applicant.create!(email: 'test@test.com', password: '123456')
         profile = Profile.create!(name: 'Aninha', social_name: 'Pedro', birth_date: '13/12/1990',
                                 qualification: 0, description: 'Sou comunicativo, gosto de responsabilidades',
@@ -18,8 +19,11 @@ feature 'Headhunter see all that applied to the job' do
         job = Job.create!(title: 'Vaga desenvolvedor', description: 'Empresa X está a procura de Juniors',
                         skills: 'Ruby on Rails', salary_from: 5000, salary_to: 10000, headhunter: headhunter, 
                         end_date: 3.days.from_now, where: 'Avenida Paulista', job_level: 0)
+        otherjob = Job.create!(title: 'Vaga suporte', description: 'Empresa Y está a procura de Juniors',
+                            skills: 'Ruby on Rails', salary_from: 5000, salary_to: 10000, headhunter: otherheadhunter,
+                            end_date: 3.days.from_now, where: 'Avenida Paulista', job_level: 0)
         apply = Apply.create!(job: job, profile: profile, cover_letter: 'Trabalhei com A há 5 anos')
-        otherapply = Apply.create!(job: job, profile: otherprofile, cover_letter: 'Trabalhei com B há 5 anos')
+        otherapply = Apply.create!(job: otherjob, profile: otherprofile, cover_letter: 'Trabalhei com B há 5 anos')
         
         login_as(headhunter, scope: :headhunter)
 
@@ -29,6 +33,6 @@ feature 'Headhunter see all that applied to the job' do
         click_on 'Candidatos'
 
         expect(page).to have_content(profile.name)
-        expect(page).to have_content(otherprofile.name)
+        expect(page).not_to have_content(otherprofile.name)
     end    
 end
